d4af9d36867bd4c05cc6a1b5c4ac94fc6724db43
Arifan28 <m.arifan28@gmail.com>
2026-04-10 15:27:24 +0700

    psuh baru

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>tes baru lagi boss</title>
+        <title>Ahhay</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
