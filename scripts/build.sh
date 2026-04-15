efdf30768f7db3ef59f45e5b4a62a7e85308def8
Prakasa <prakasa@devetek.com>
2026-04-15 16:15:35 +0700

    fix: update octane version

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,17 +2,19 @@
 
 # We don't care about exposed environment variables, it just demo repository!
 
+# install all dependencies veforee creating cache, so that we can optimize autoloader
+composer install
+
+# laravel optimizations
 php artisan key:generate | echo "key:generate already exist"
-
 php artisan optimize:clear | echo "No cache need to clear"
-
-# install dependencies
-composer install --optimize-autoloader --no-dev
 php artisan storage:link | echo "storage:link alrady exist"
 php artisan config:clear
 php artisan route:clear
 php artisan cache:clear
-# php artisan queue:flush | echo "ignore error queue:flush"
+
+# composer install for production dependencies only
+composer install --optimize-autoloader --no-dev
 
 # re-create cache
 php artisan optimize
